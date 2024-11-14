6be09317a6f274e03d1543d445420aeecce82063
gerimuti <136038547+gerimuti@users.noreply.github.com>
2024-11-14 18:25:42 +0700

    AUTH BERHASIL

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\api\PersonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ResepController;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 
@@ -20,8 +20,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('/person', [PersonController::class], 'person');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LogoutController::class, 'logout'])->middleware('auth:sanctum');
+
+//Route::resource('/person', [PersonController::class], 'person');
+
+Route::post('/register', App\Http\Controllers\api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+// Route::post('/register', [RegisterController::class, 'register']);
+// Route::post('/login', [LoginController::class, 'login']);
+// Route::post('/logout', [LogoutController::class, 'logout'])->middleware('auth:sanctum');
